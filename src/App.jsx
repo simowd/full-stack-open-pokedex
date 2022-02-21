4047d06de32951fab228f0bac503dcebd9966291
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-02-21 15:21:55 -0400

    #skip

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 })))
 
 const App = () => {
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
-  if (isLoading) {
+  const { data: pokemonList, error, isLoadin } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
+  if (isLoadin) {
     return <LoadingSpinner />
   }
   if (error) {
